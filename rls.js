9284fa85c9b20fca1d2955001a7abeedd105a6d9
SpiralV <MVelasco90@hotmail.com>
2021-05-14 16:01:01 -0700

    html css js skeleton assembled

---
+++ rls.js
@@ -0,0 +1,21 @@
+/* sudocode
+
+let boardspace
+let playerIcon
+let start
+let stop 
+let reset
+class entire menu bar? lol
+class blocks? for arrays?
+make object generator for blocks
+make blocks disappear at bottom if not shot
+google border collision in javascript lol
+check out canvas
+score incrementor
+shoot mapped to button
+setinterval shoot
+left right mapped to button
+
+
+
+*/
