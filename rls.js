fcb9429416786b2afdd761c89cbdc37fbfce408d
SpiralV <MVelasco90@hotmail.com>
2021-05-17 16:43:42 -0700

    attached canvas, day 1 notes added to js

--- rls.js
+++ rls.js
@@ -1,20 +1,57 @@
+const canvas = document.getElementById('canvas1')
+const contx = canvas.getContext('2d')
+canvas.width = 1000
+canvas.height = 563
+
+let 
+// TODO basic js setup -- see fishgame(js-2dgame-yt) for help, dont be afraid to google
+// TODO player movement and shoot buttons
+// TODO block generation (simple squares for now)
+// TODO block styling(find sprites and draw hitboxes)
+// TODO collision detection (shoot/player/block/border)
+// TODO menu buttons/options
+// TODO reconsider tetris shapes (pending further review of hitboxes)
+
+// /!\ stretch goals
+// blocks take 4 hits to destroy, scroll speeds balanced accordingly
+// scroll illusion
+// style everything better
+// for example, blocks have hitbars or visually represent life status via each block lighting up or dimming preferably at random
+// ie. major spritework
+// sound effects
+// background music (toggle button)
+// full grid movement or mouse movement
+// snap blocks into tetris formats and fall/rise in grid patterns only
+// score numerical styling based on tetris
+// scoring based on dodging straight bars specifically
+// high score leaderboard + initials entry
+// additional menu option considerations and css stylings to facilitate game modes/window focus
+// convoluted 1P endless mode with levels akin to tetris, and lives upon hi-score thresholds
+// convoluted 2P co-op mode where other player plays tetris, and the scoring is based on multiple bar types
+// easter egg to unlock tempest view mode, and extremely convoluted 1p mode
+// extremely convoluted 1P mode where blocks have elements and attacks + buffs/debuffs
+// extremely convoluted 1P mode has convoluted plotline about humanity 
+
 /* sudocode
 
-let boardspace
-let playerIcon
-let start
-let stop 
-let reset
-class entire menu bar? lol
-class blocks? for arrays?
-make object generator for blocks
-make blocks disappear at bottom if not shot
-google border collision in javascript lol
-check out canvas
-score incrementor
-shoot mapped to button
-setinterval shoot
-left right mapped to button
+///// not quite answered yet
+dont forget to google codepen vertshoot game
+setinterval shoot -- investigating timestops in javascript games
+shoot mapped to button -- double check js connectors
+left right mapped to button -- same as above (mouse operation stretch goal)
+fish game  |google border collision in javascript lol - fishgame good example, to be explored further
+score incrementor - score++ my friend (iterations stretch goal)
+
+/ / / / / answered / / / / /
+*fish game* |make blocks disappear at bottom if not shot - check fishgame
+*canvas*  |let boardspace - canvassed
+ canvas   |let playerIcon? - affix img to hitbox
+ canvas   |let start/let stop/let reset - menu bar
+ canvas   |js class entire menu bar? lol - menu bar/right canvas
+ js class blocks? for arrays? - yes class Blocks blocksArray
+ make object generator for blocks - remember constructor() first
+ check out canvas - definitely done
+
 
 
 
